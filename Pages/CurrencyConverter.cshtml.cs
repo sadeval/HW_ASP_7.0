9fcc2b218c8fd803ee459e2ceb901da1ebcbbe3c
sadeval <sadeval2011@gmail.com>
2024-11-30 15:12:05 +0200

    Add project files.

---
+++ Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW_ASP_7._0.Pages
+{
+    public class CurrencyConverterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
